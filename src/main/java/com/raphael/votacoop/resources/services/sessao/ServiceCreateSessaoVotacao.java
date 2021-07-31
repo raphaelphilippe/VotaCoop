82e9c110406c6b1815b885fa240b464bead9febf
Raphael Philippe <almeidasilva.raphael@gmail.com>
2021-07-31 13:46:09 -0300

    Ajustes nas validações da Sessão e inclusão de decumentação Swagger.

--- src/main/java/com/raphael/votacoop/resources/services/sessao/ServiceCreateSessaoVotacao.java
+++ src/main/java/com/raphael/votacoop/resources/services/sessao/ServiceCreateSessaoVotacao.java
@@ -14,7 +14,7 @@ import com.raphael.votacoop.resources.services.pauta.ServiceFindPautas;
 @Service
 public class ServiceCreateSessaoVotacao {
 	
-	final String prazoPadraoSessao = "01:00";
+	final String prazoPadraoSessao = "00:01";
 	
 	
 	@Autowired
@@ -24,50 +24,59 @@ public class ServiceCreateSessaoVotacao {
 	private ServiceFindPautas serviceFindPautas;
 	
 	
-	public SessaoVotacao create(Integer idPauta, String prazoSessao){
-		
-		if(checkPautaAlreadyHasSesssao(idPauta)) {
-			
-			if(checkFormatPrazoSessao(prazoSessao)) {
-				
-				SessaoVotacao sessaoVotacao = new SessaoVotacao(
-						null, 
-						LocalDateTime.now(), 
-						(prazoSessao == null ? prazoPadraoSessao : prazoSessao), 
-						serviceFindPautas.findById(idPauta)
-						);
-				
-				return sessaoVotacaoRepository.save(sessaoVotacao);
+	public SessaoVotacao create(Integer idPauta, String prazoSessao) {
+
+		if (checkPautaAlreadyHasSesssao(idPauta)) {
+
+			if ( ! prazoSessao.equals("") && ! prazoSessao.equals("00:00") ) {
+				checkFormatPrazoSessao(prazoSessao);
+			} else {
+				prazoSessao = prazoPadraoSessao;
 			}
+
+			SessaoVotacao sessaoVotacao = new SessaoVotacao(
+					null,
+					LocalDateTime.now(),
+					prazoSessao,
+					serviceFindPautas.findById(idPauta)
+			);
+
+			return sessaoVotacaoRepository.save(sessaoVotacao);
 		}
-			
+
 		return null;
 	}
 	
 	
 	private boolean checkFormatPrazoSessao(String prazoSessao) {
-		
-		if(prazoSessao.length() != 5) {
-			throw new ValidationsException("O prazo da Sessão de Votação precisa conter 5 caracteres no seguinte formato: HH:mm.");
+
+		if (prazoSessao.length() != 5) {
+			throw new ValidationsException(
+					"O prazo da Sessão de Votação precisa conter 5 caracteres no seguinte formato: HH:mm.");
 		}
-		
-		if(! prazoSessao.contains(":")) {
-			throw new ValidationsException("O prazo da Sessão de Votação precisa conter o sinal de dois pontos entre as horas e os minutos no seguinte formato: HH:mm.");
+
+		if (!prazoSessao.contains(":")) {
+			throw new ValidationsException(
+					"O prazo da Sessão de Votação precisa conter o sinal de dois pontos entre as horas e os minutos no seguinte formato: HH:mm.");
 		}
-		
+
 		String[] listPrazoSessao = prazoSessao.split(":");
-		if(! listPrazoSessao[0].matches("[0-9]*")) {
-			throw new ValidationsException("No prazo da Sessão de Votação as horas precisam conter um valor numérico entre 0 e 9 no seguinte formato: HH:mm.");
+		if (!listPrazoSessao[0].matches("[0-9]*")) {
+			throw new ValidationsException(
+					"No prazo da Sessão de Votação as horas precisam conter um valor numérico entre 0 e 9 no seguinte formato: HH:mm.");
 		}
-		
-		if( (! listPrazoSessao[1].matches("[0-9]*") ) | (Integer.parseInt(listPrazoSessao[1]) > 59 | Integer.parseInt(listPrazoSessao[1]) < 00)) {
-			throw new ValidationsException("No prazo da Sessão de Votação os minutos precisam conter até dois algarismos entre 0 e 9 e ser um número entre 00 e 59 no seguinte formato HH:mm.");
+
+		if ((!listPrazoSessao[1].matches("[0-9]*"))
+				| (Integer.parseInt(listPrazoSessao[1]) > 59 | Integer.parseInt(listPrazoSessao[1]) < 00)) {
+			throw new ValidationsException(
+					"No prazo da Sessão de Votação os minutos precisam conter até dois algarismos entre 0 e 9 e ser um número entre 00 e 59 no seguinte formato HH:mm.");
 		}
-		
-		if(prazoSessao == "00:00") {
-			throw new ValidationsException("Não é possível iniciar um sessão com o um prazo de 00:00. Caso um prazo válido não seja informado, a sessão terá a duração de 1 minuto.");
+
+		if (prazoSessao == "00:00") {
+			throw new ValidationsException(
+					"Não é possível iniciar um sessão com o um prazo de 00:00. Caso um prazo válido não seja informado, a sessão terá a duração de 1 minuto.");
 		}
-		
+
 		return true;
 	}
 	
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
-insert into Usuario (id, nome, cpf, email) values (1, 'Raphael', '05888397792', 'raphael@gmail.com');
-insert into Usuario (id, nome, cpf, email) values (2, 'Andrea', '10626365708', 'andrea@gmail.com');
-insert into Usuario (id, nome, cpf, email) values (3, 'Virginia', '72571977768', 'virginia@gmail.com');
-insert into Usuario (id, nome, cpf, email) values (4, 'Batista', '26755365708', 'batista@gmail.com');
-insert into Usuario (id, nome, cpf, email) values (5, 'Bruno', '11327036703', 'bruno@gmail.com');
+insert into Usuario (id, nome, cpf, email) values (1, 'Raphael', '96887565048', 'raphael@gmail.com');
+insert into Usuario (id, nome, cpf, email) values (2, 'Andrea', '21518556000', 'andrea@gmail.com');
+insert into Usuario (id, nome, cpf, email) values (3, 'Virginia', '90067359060', 'virginia@gmail.com');
+insert into Usuario (id, nome, cpf, email) values (4, 'Batista', '88260001071', 'batista@gmail.com');
+insert into Usuario (id, nome, cpf, email) values (5, 'Bruno', '46942410012', 'bruno@gmail.com');
 
-insert into Pauta (id, data_Criacao_Pauta, descricao, status_Pauta) values (1, '2021-07-26T01:07:00.000', 'ppppppppppppppppppppp1', 1)
+insert into Pauta (id, data_Criacao_Pauta, descricao, status_Pauta) values (1, '2021-07-26T01:07:00.000', 'PAUTA 1 #########################', 1)
 insert into Sessao_Votacao (pauta_id, data_hora_inicio_sessao, tempo_prazo) values (1, '2021-07-26T01:11:00.000', '01:02')
 
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (1, '2021-07-26T01:16:00.000', 1, 1, 1)
@@ -12,23 +12,10 @@ insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (2, '2
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (3, '2021-07-26T01:18:00.000', 0, 1, 3)
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (4, '2021-07-26T01:19:00.000', 0, 1, 4)
 
-insert into Pauta (id, data_Criacao_Pauta, descricao, status_Pauta) values (2, '2021-07-26T01:07:00.000', 'ppppppppppppppppppppp 2222', 1)
+insert into Pauta (id, data_Criacao_Pauta, descricao, status_Pauta) values (2, '2021-07-26T01:07:00.000', 'PAUTA 2 #########################', 1)
 insert into Sessao_Votacao (pauta_id, data_hora_inicio_sessao, tempo_prazo) values (2, '2021-07-26T01:11:00.000', '01:02')
 
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (5, '2021-07-26T01:16:00.000', 1, 2, 1)
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (6, '2021-07-26T01:17:00.000', 1, 2, 2)
-
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (9, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (10, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (11, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (12, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (13, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (14, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (15, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (16, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (17, '2021-07-26T01:17:00.000', 1, 2, 2)
-insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (18, '2021-07-26T01:17:00.000', 1, 2, 2)
-
-
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (7, '2021-07-26T01:18:00.000', 0, 2, 3)
 insert into voto (id, data_hora_voto, voto, id_sessao, id_usuario) values (8, '2021-07-26T01:19:00.000', 0, 2, 4)
